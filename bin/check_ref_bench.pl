07fc2a6f8de40a92344d35412e160fd418974be0
Alessandro Vullo <avullo@ebi.ac.uk>
2013-10-25 12:28:22 +0100

    Modified plan to accomodate assert_ref test

---
+++ bin/check_ref_bench.pl
@@ -0,0 +1,76 @@
+#!perl 
+use strict;
+use warnings;
+
+################################################
+#                                              #
+# Benchmark                                    #
+#                                              #
+# Bio::EnsEMBL::XS::Utils::Scalar::check_ref   #
+#  vs                                          #
+# Bio::EnsEMBL::Utils::Scalar::check_ref       #
+#                                              #
+################################################
+
+use List::Util qw(shuffle);
+use Benchmark;
+
+use Bio::EnsEMBL::Utils::Scalar;
+use Bio::EnsEMBL::XS;
+
+$| = 1;
+
+#
+# Benchmark using large artificial argument list
+#
+print '-' x 18, "\n 1M argument list\n", '-' x 18, "\n\n";
+my @args = ("0000001" .. "1000000");
+
+my @random_args;
+for my $i (shuffle @args) {
+  push @random_args, "-$i", $i;
+}
+
+print "[Bio::EnsEMBL::Utils::Argument]\t\t";
+my $start = new Benchmark;
+my @args1 = Bio::EnsEMBL::Utils::Argument::rearrange_pp([@args], @random_args);
+my $end = new Benchmark;
+my $diff = timediff($end, $start);
+printf "time taken was %s seconds\n", timestr($diff, 'all');
+
+print "[Bio::EnsEMBL::XS::Utils::Argument]\t";
+$start = new Benchmark;
+my @args2 = Bio::EnsEMBL::XS::Utils::Argument::rearrange([@args], @random_args);
+$end = new Benchmark;
+$diff = timediff($end, $start);
+printf "time taken was %s seconds\n\n", timestr($diff, 'all');
+
+#
+# Benchmark calling 10K times
+#
+print '-' x 16, "\n Call 10K times\n", '-' x 16, "\n\n";
+my $keys = [qw/one two three four five six seven eight nine ten/];
+
+print "[Bio::EnsEMBL::Utils::Argument] ";
+my $i = 1;
+$start = new Benchmark;
+for (1 .. 10000) {
+  my @two_output = Bio::EnsEMBL::Utils::Argument::rearrange_pp($keys, (-SIX => 6, -THrEE => 3));
+  my @six_output = Bio::EnsEMBL::Utils::Argument::rearrange_pp($keys, (-SIX => 6, -THREE => 3, -ten => 10, -four => 4, -ONE => 1, -TWO => 2));
+  print '.' unless $i++ % 1000;
+}
+$end = new Benchmark;
+$diff = timediff($end, $start);
+printf "\nTime taken was %s seconds\n\n", timestr($diff, 'all');
+
+print "[Bio::EnsEMBL::XS::Utils::Argument] ";
+$i = 1;
+$start = new Benchmark;
+for (1 .. 10000) {
+  my @two_output = Bio::EnsEMBL::XS::Utils::Argument::rearrange($keys, (-SIX => 6, -THrEE => 3));
+  my @six_output = Bio::EnsEMBL::XS::Utils::Argument::rearrange($keys, (-SIX => 6, -THREE => 3, -ten => 10, -four => 4, -ONE => 1, -TWO => 2));
+  print '.' unless $i++ % 1000; 
+}
+$end = new Benchmark;
+$diff = timediff($end, $start);
+printf "\nTime taken was %s seconds\n\n", timestr($diff, 'all');
