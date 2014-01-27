62236e5055af32260384fd2cd969765aaaa2f63a
Alessandro Vullo <avullo@ebi.ac.uk>
2014-01-27 11:18:46 +0000

    Ensembl helpdesk contact details changed

--- lib/Bio/EnsEMBL/XS.pm
+++ lib/Bio/EnsEMBL/XS.pm
@@ -20,10 +20,10 @@ limitations under the License.
 =head1 CONTACT
 
   Please email comments or questions to the public Ensembl
-  developers list at <dev@ensembl.org>.
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
 
   Questions may also be sent to the Ensembl help desk at
-  <helpdesk@ensembl.org>.
+  <http://www.ensembl.org/Help/Contact>.
 
 =cut
 
@@ -119,9 +119,9 @@ No known bugs at the moment. Development in progress.
 =head1 SUPPORT
 
 Please email comments or questions to the public Ensembl
-developers list at <dev@ensembl.org>.
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
 
 Questions may also be sent to the Ensembl help desk at
-<helpdesk@ensembl.org>.
+<http://www.ensembl.org/Help/Contact>.
 
 1; # End of Bio::EnsEMBL::XS
